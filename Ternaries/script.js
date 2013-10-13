bffba25ddf246aa6c4be70a3b7d158aa32f7d8e9
Harmony Cornett <harmony@fullsail.edu>
2013-10-13 15:25:32 -0400

    Ternaries
    screencast

---
+++ Ternaries/script.js
@@ -0,0 +1,25 @@
+//Conditional Logic - Ternary Operators
+
+var gpa = 48;
+
+//if the gpa is over the min 2.0 score, the student can graduate
+if( gpa > 2.0){
+	console.log("You can graduate!")
+}else{
+	console.log("GPA is too low!");
+}*/
+
+(gpa > 2.0) ? console.log("you can graduate!")
+
+
+
+
+
+
+
+var age = 11;
+var book;
+
+//if the child is under 10, they get Green Eggs and Ham, otherwise they get the Time Machine.
+book = (age< 10)
+console.log(book);
